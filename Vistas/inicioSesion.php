3d8b58ba6e40219d6c7d335b7f8921d84576433f
unknown <psaenzguerrero@gmail.com>
2025-01-21 13:05:26 +0100

    primeros pasos

---
+++ Vistas/inicioSesion.php
@@ -0,0 +1,11 @@
+<body>
+    <form action="" method="post">
+        <label for="nombre">Escribe tu nombre de usuario</label>
+        <input type="text" name="nombreU" value="nombreU">
+        <br>
+        <label for="nombre">Escribe tu contraseña</label>
+        <input type="text" name="contraseñaU" value="contraseñaU">
+        <br>
+        <input type="submit" value="verificarUsu" name="action">
+    </form>
+</body>
